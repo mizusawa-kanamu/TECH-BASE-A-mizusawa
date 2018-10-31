246639a2cf6b836dccce5638254fb77481b09742
mizusawa-kanamu <42557515+mizusawa-kanamu@users.noreply.github.com>
2018-10-31 20:38:18 +0900

    Update mission_6-1_DBconnect.php

--- mission_6-1_DBconnect.php
+++ mission_6-1_DBconnect.php
@@ -3,9 +3,9 @@
 //mission_6-1 データベース接続プログラム
 
 //データベース接続
-$dsn = 'mysql:dbname=tt_251_99sv_coco_com;host=localhost';
-$user = 'tt-251.99sv-coco.com'; 
-$password = 'b6E3Phcg';
+$dsn = 'データベース名';
+$user = 'ユーザ名'; 
+$password = 'パスワード';
 $pdo = new PDO($dsn,$user,$password);
 
 //例外をスローしてくれる
